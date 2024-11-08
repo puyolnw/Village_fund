9bd9887afaebff9b09136ce3cabf1297fc73fe97
unknown <gamemega2546@gmail.com>
2024-11-08 18:30:25 +0700

    first commit

---
+++ src/data/activity-chart.ts
@@ -0,0 +1,11 @@
+export type TransactionDataType = { day: string; deposit: number; withdraw: number }[];
+
+export const transactionData: TransactionDataType = [
+  { day: 'ส', deposit: 420, withdraw: 220 },
+  { day: 'อา', deposit: 332, withdraw: 132 },
+  { day: 'จ', deposit: 301, withdraw: 251 },
+  { day: 'อ', deposit: 334, withdraw: 334 },
+  { day: 'พ', deposit: 490, withdraw: 390 },
+  { day: 'พฤ', deposit: 160, withdraw: 230 },
+  { day: 'ศ', deposit: 320, withdraw: 320 },
+];
